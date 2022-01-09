2213dc991f4e248a1fc8e6cc2f50b2d262dd0ff9
zmdiv <66516043+zmdiv@users.noreply.github.com>
2022-01-09 21:07:04 +0100

    Update 5 Kyu - Is my friend cheating?.py

--- 5 Kyu - Is my friend cheating?.py
+++ 5 Kyu - Is my friend cheating?.py
@@ -6,10 +6,14 @@ Given a number n, could you tell me the numbers he excluded from the sequence?''
 def remov_nb(n):
     lst = []  # final list
     num_list = [x for x in range(1, n + 1)]  # list with all numbers
-
+    f_lst = []
     for a in range(1, n + 1):
         for b in range(1, n + 1):
             if a * b == sum(num_list) - a - b:
                 lst.append(a)
-                lst.append(b)
-    return lst
+                lst.append(b)             
+    try:
+        f_lst = [(lst[0], lst[1]), (lst[2], lst[3])]
+    except:
+        pass
+    return f_lst
