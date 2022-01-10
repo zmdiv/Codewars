77438ecd0780eab35caf6b45e3c22da5f7c562c2
zmdiv <66516043+zmdiv@users.noreply.github.com>
2022-01-10 22:22:10 +0100

    Update and rename 5 Kyu - Is my friend cheating?.py to Is my friend cheating?.py

--- Is my friend cheating?.py
+++ Is my friend cheating?.py
@@ -3,8 +3,10 @@ Within that sequence, he chooses two numbers, a and b.
 He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
 Given a number n, could you tell me the numbers he excluded from the sequence?'''
 
+# version 1. Correct version, calculations are OK, but the test is failed due to high execution time (O(n**2))
+
 def remov_nb(n):
-    lst = []  # final list
+    lst = []   
     num_list = [x for x in range(1, n + 1)]  # list with all numbers
     f_lst = []
     for a in range(1, n + 1):
@@ -17,3 +19,16 @@ def remov_nb(n):
     except:
         pass
     return f_lst
+
+
+# version 2. Efficient, test passed - O(n)
+
+def remov_nb(n):
+    result = []
+    # Sn=n(a1 + a2)2  - Sum of the Terms of an Arithmetic Sequence (Arithmetic Series), where n is the number of terms, a1 is the first term and an is the last term.
+    sum_nums = n * (n + 1) // 2
+    for a in range(1, n+1):
+        b = (sum_nums - a) // (a + 1) # taking into consideration that (sum_nums - a - b = a * b) that means -->> b = (sum_nums - a) / (a + 1)
+        if b <= n and sum_nums - a - b == a * b:
+            result.append((a, b))
+    return result
